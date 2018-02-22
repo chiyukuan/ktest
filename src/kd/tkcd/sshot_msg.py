00416cdcade18eebc9b1b56c7f79ba7f309d2275
Ray Kuan <ray@kodiakdata.com>
2018-02-21 17:03:51 -0800

    update

--- src/kd/tkcd/sshot_msg.py
+++ src/kd/tkcd/sshot_msg.py
@@ -7,12 +7,12 @@ from kd.tkcd import TKCD_CMD_TYPE
 logger = getLogger(__name__)
 
 def getAddSShotMsg(cltOpq, readonly, panelIds):
-    panelCnt = len(panelIds)
-    pLen  = 40
-    if panelCnt > 7:
-        pLen += (panelCnt - 7) * 4
-    msg = SshotMsg(0xF3F3, 0, 0, pLen, TKCD_CMD_TYPE.ADD_SNAPSHOT.getVal(), 0, 0, cltOpq,
-                   readonly, 0, len(panelIds))
+    slotCnt  = len(panelIds)
+    if slotCnt < 7:
+        slotCnt = 7
+    pLen = 40 + (slotCnt - 7) * 4
+    msg = eval('SshotMsg_%d' % slotCnt)(0xF3F3, 0, 0, pLen, TKCD_CMD_TYPE.ADD_SNAPSHOT.getVal(), 0, 0, cltOpq,
+                       readonly, 0, len(panelIds))
     for idx in range( len(panelIds) ):
         msg.panelIds[idx] = panelIds[idx]
     return msg
@@ -35,8 +35,7 @@ class SshotMsg(Structure):
                 ("cltOpq",      c_uint),
                 ("readOnly",    c_ubyte),
                 ("padding",     c_ubyte),
-                ("panelCnt",    c_ushort),
-                ("panelIds",    c_uint * 8)]
+                ("panelCnt",    c_ushort)]
 
     def __str__(self):
         if self.rc == 0:
@@ -54,6 +53,49 @@ class SshotMsg(Structure):
                 self.rc)
 
     __repr__ = __str__
+
+class SshotMsg_7(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 7)]
+
+class SshotMsg_8(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 8)]
+
+class SshotMsg_9(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 9)]
+
+class SshotMsg_10(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 10)]
+
+class SshotMsg_11(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 11)]
+
+class SshotMsg_12(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 12)]
+
+class SshotMsg_13(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 13)]
+
+class SshotMsg_14(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 14)]
+
+class SshotMsg_15(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 15)]
+
+class SshotMsg_16(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 16)]
+
+class SshotMsg_17(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 17)]
+
+class SshotMsg_18(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 18)]
+
+class SshotMsg_19(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 19)]
+
+class SshotMsg_20(SshotMsg):
+    _fields_ = [ ("panelIds",    c_uint * 20)]
+
 
 if __name__ == '__main__':
     ''' Test this module here '''
