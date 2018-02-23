ca0d4dd50874c62a40bf076a4cf6bfb8eea3820b
chiyukuan <raykuan@gmail.com>
2018-02-23 13:08:29 -0800

    update

--- src/kd/tkcd/sshot_msg.py
+++ src/kd/tkcd/sshot_msg.py
@@ -6,22 +6,24 @@ from kd.tkcd import TKCD_CMD_TYPE
 
 logger = getLogger(__name__)
 
-def getAddSShotMsg(cltOpq, readonly, panelIds):
-    slotCnt  = len(panelIds)
-    if slotCnt < 7:
-        slotCnt = 7
-    pLen = 40 + (slotCnt - 7) * 4
+def getAddSShotMsg(cltOpq, readonly, entries):
+    slotCnt  = len(entries)
+    pLen = 32 + slotCnt * 8
     msg = eval('SshotMsg_%d' % slotCnt)(0xF3F3, 0, 0, pLen, TKCD_CMD_TYPE.ADD_SNAPSHOT.getVal(), 0, 0, cltOpq,
-                       readonly, 0, len(panelIds))
-    for idx in range( len(panelIds) ):
-        msg.panelIds[idx] = panelIds[idx]
+                       0xcacacacadeafbeaf, 0, readonly, 0, len(entries), 0)
+    for idx in range( len(entries) ):
+        msg.entries[2 * idx    ] = entries[idx][0]
+        msg.entries[2 * idx + 1] = entries[idx][1]
     return msg
 
-def getDelSShotMsg(cltOpq, readonly, panelIds):
-    msg = SshotMsg(0xF3F3, 0, 0, 40, TKCD_CMD_TYPE.DEL_SNAPSHOT.getVal(), 0, 0, cltOpq,
-                   readonly, 0, len(panelIds))
-    for idx in range( len(panelIds) ):
-        msg.panelIds[idx] = panelIds[idx]
+def getAddSShotMsg(cltOpq, readonly, entries):
+    slotCnt  = len(entries)
+    pLen = 32 + slotCnt * 8
+    msg = eval('SshotMsg_%d' % slotCnt)(0xF3F3, 0, 0, pLen, TKCD_CMD_TYPE.DEL_SNAPSHOT.getVal(), 0, 0, cltOpq,
+                       0xcacacacadeafbeaf, 0, readonly, 0, len(entries), 0)
+    for idx in range( len(entries) ):
+        msg.entries[2 * idx    ] = entries[idx][0]
+        msg.entries[2 * idx + 1] = entries[idx][1]
     return msg
 
 class SshotMsg(Structure):
@@ -33,9 +35,12 @@ class SshotMsg(Structure):
                 ("msgOpq",      c_ubyte),
                 ("status",      c_ushort),
                 ("cltOpq",      c_uint),
+                ("panelSeqNo",  c_ulonglong),
+                ("tileSeqNo",   c_ulonglong),
                 ("readOnly",    c_ubyte),
                 ("padding",     c_ubyte),
-                ("panelCnt",    c_ushort)]
+                ("entryCnt",    c_ushort),
+                ("padding2",    c_uint)]
 
     def __str__(self):
         if self.rc == 0:
@@ -43,63 +48,62 @@ class SshotMsg(Structure):
             return "send %s-%s cmd %d-panels: %s" % (
                     "read-only" if bool(self.readOnly) else "read-write",
                     TKCD_CMD_TYPE.getEnum(self.cmdType),
-                    self.panelCnt,
-                    ','.join(str( self.panelIds[idx] ) for idx in range(self.panelCnt)) )
+                    self.entryCnt,
+                    ','.join( "%d->%d" % (self.entries[2 * idx], self.entries[2 * idx + 1] )for idx in range(self.entryCnt)) )
         return "recv %s-%s rsp %d-panels: %s with rc %u" % (
                 "read-only" if bool(self.readOnly) else "read-write",
                 TKCD_CMD_TYPE.getEnum(self.cmdType),
-                self.panelCnt,
-                ','.join(str( self.panelIds[idx] ) for idx in range(self.panelCnt)),
+                self.entryCnt,
+                ','.join( "%d->%d" % (self.entries[2 * idx], self.entries[2 * idx + 1] )for idx in range(self.entryCnt)),
                 self.rc)
 
     __repr__ = __str__
 
+class SshotMsg_1(SshotMsg):
+    _fields_ = [ ("entries",    c_uint * 2)]
+
+class SshotMsg_2(SshotMsg):
+    _fields_ = [ ("entries",    c_uint * 4)]
+
+class SshotMsg_3(SshotMsg):
+    _fields_ = [ ("entries",    c_uint * 6)]
+
+class SshotMsg_4(SshotMsg):
+    _fields_ = [ ("entries",    c_uint * 8)]
+
+class SshotMsg_5(SshotMsg):
+    _fields_ = [ ("entries",    c_uint * 10)]
+
+class SshotMsg_6(SshotMsg):
+    _fields_ = [ ("entries",    c_uint * 12)]
+
 class SshotMsg_7(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 7)]
+    _fields_ = [ ("entries",    c_uint * 14)]
 
 class SshotMsg_8(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 8)]
+    _fields_ = [ ("entries",    c_uint * 16)]
 
 class SshotMsg_9(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 9)]
+    _fields_ = [ ("entries",    c_uint * 18)]
 
 class SshotMsg_10(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 10)]
+    _fields_ = [ ("entries",    c_uint * 20)]
 
 class SshotMsg_11(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 11)]
+    _fields_ = [ ("entries",    c_uint * 22)]
 
 class SshotMsg_12(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 12)]
+    _fields_ = [ ("entries",    c_uint * 24)]
 
 class SshotMsg_13(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 13)]
-
-class SshotMsg_14(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 14)]
+    _fields_ = [ ("entries",    c_uint * 26)]
 
 class SshotMsg_15(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 15)]
-
-class SshotMsg_16(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 16)]
-
-class SshotMsg_17(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 17)]
-
-class SshotMsg_18(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 18)]
-
-class SshotMsg_19(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 19)]
-
-class SshotMsg_20(SshotMsg):
-    _fields_ = [ ("panelIds",    c_uint * 20)]
-
+    _fields_ = [ ("entries",    c_uint * 28)]
 
 if __name__ == '__main__':
     ''' Test this module here '''
-    msg = getAddSShotMsg(0xcacacaca, True, (11,12,13,14,15,15,16,18))
+    msg = getAddSShotMsg(0xcacacaca, True, [(11 + idx, 21 + idx) for idx in range(8)] )
     print msg
     msg.rc = 1
     print msg
